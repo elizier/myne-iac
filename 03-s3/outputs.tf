4b4c9041726c6c5a11dea0e515d4302934d71a0a
jonatasrenan <jonatas@dcc.ufmg.br>
2021-11-14 12:18:36 -0300

    adding s3 outputs

---
+++ 03-s3/outputs.tf
@@ -0,0 +1,32 @@
+output "app_myne_bucket_regional_name" {
+  value = aws_s3_bucket.app_myne.bucket_regional_domain_name
+}
+
+output "audios_myne_bucket_regional_name" {
+  value = aws_s3_bucket.audios_myne.bucket_regional_domain_name
+}
+
+output "files_myne_bucket_regional_name" {
+  value = aws_s3_bucket.files_myne.bucket_regional_domain_name
+}
+
+output "images_myne_bucket_regional_name" {
+  value = aws_s3_bucket.images_myne.bucket_regional_domain_name
+}
+
+output "_myne_bucket_regional_name" {
+  value = aws_s3_bucket._myne.bucket_regional_domain_name
+}
+
+output "pdf_myne_bucket_regional_name" {
+  value = aws_s3_bucket.pdf_myne.bucket_regional_domain_name
+}
+
+output "userphotos_myne_bucket_regional_name" {
+  value = aws_s3_bucket.userphotos_myne.bucket_regional_domain_name
+}
+
+output "videos_myne_bucket_regional_name" {
+  value = aws_s3_bucket.videos_myne.bucket_regional_domain_name
+}
+
