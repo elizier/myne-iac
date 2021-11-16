d9434834749747cac1b721422cad3b778a82ac8a
jonatasrenan <jonatas@dcc.ufmg.br>
2021-11-16 18:55:43 -0300

    removing egress from sgr postgres.

--- 02-sgr/_postgres.tf
+++ 02-sgr/_postgres.tf
@@ -18,12 +18,5 @@ resource "aws_security_group" "postgres" {
     cidr_blocks = ["0.0.0.0/0"]
 #    cidr_blocks = ["168.121.77.226/32"]
   }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
 
