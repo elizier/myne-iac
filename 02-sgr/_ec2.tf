9116b02c6c3b41cfa684ae281d3263b06b69d050
jonatasrenan <jonatas@dcc.ufmg.br>
2021-11-17 19:57:29 -0300

    sgr alb

--- 02-sgr/_ec2.tf
+++ 02-sgr/_ec2.tf
@@ -4,33 +4,12 @@ resource "aws_security_group" "ec2" {
 
   vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
 
-  # ingress {
-  #   from_port   = 80
-  #   to_port     = 80
-  #   protocol    = "tcp"
-  #   cidr_blocks = ["0.0.0.0/0"]
-  # }
-
   ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  # ingress {
-  #   from_port   = 443
-  #   to_port     = 443
-  #   protocol    = "tcp"
-  #   cidr_blocks = ["0.0.0.0/0"]
-  # }
-
-  # ingress {
-  #   from_port   = 8443
-  #   to_port     = 8443
-  #   protocol    = "tcp"
-  #   cidr_blocks = ["0.0.0.0/0"]
-  # }
 
   ingress {
     from_port   = 9990
--- 02-sgr/output.tf
+++ 02-sgr/output.tf
@@ -5,3 +5,7 @@ output "postgres_sec_group_id" {
 output "ec2_sec_group_id" {
   value = aws_security_group.ec2.id
 }
+
+output "public_sec_group_id" {
+  value = aws_security_group.ec2.id
+}
