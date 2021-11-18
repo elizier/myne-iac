46fe3d9b60b5d01864d93ded5aea392004d8e6df
jonatasrenan <jonatas@dcc.ufmg.br>
2021-11-18 18:52:38 -0300

    r53: add www records.

--- 08-r53/_www_myne_digital-dns.tf
+++ 08-r53/_www_myne_digital-dns.tf
@@ -1,15 +1,4 @@
-# resource "aws_route53_record" "www_myne_front" {
-#   zone_id = data.terraform_remote_state.zone.outputs.public_zone_id
-#   name    = "www"
-#   type    = "A"
-
-#   alias {
-#     name                   = data.terraform_remote_state.cdn.outputs.www_domain_name
-#     zone_id                = data.terraform_remote_state.cdn.outputs.www_hosted_zone_id
-#     evaluate_target_health = false
-#   }
-# }
-resource "aws_route53_record" "www_myne_front" {
+ resource "aws_route53_record" "www_myne_front" {
    zone_id = data.terraform_remote_state.zone.outputs.public_zone_id
    name    = "www"
    type    = "A"
